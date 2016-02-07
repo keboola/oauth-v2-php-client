35867bd285ec0c6ead8dcf83f5c7f699207d481a
Ondrej Vana <ondrej.vana@keboola.com>
2016-02-07 08:28:38 -0800

    feat: finish Manager::add()

--- src/Manager.php
+++ src/Manager.php
@@ -8,24 +8,34 @@ use GuzzleHttp\Client;
  */
 class Manager extends Common
 {
+    protected $requiredApiDetails = [
+        "component_id",
+        "friendly_name",
+        "app_key",
+        "app_secret",
+        "auth_url",
+        "token_url",
+        "oauth_version"
+    ];
+
     public function __construct($manageToken)
     {
         $this->client = $this->getClient(['X-KBC-ManageApiToken' => $manageToken]);
     }
 
     /**
-     * @param string $componentId ## more than that!
+     * @param array $details ## more than that!
      * @param string $kbcToken ## TODO remove once docker encryption can do image encryption w/o user token
      */
-    public function add($componentId, $kbcToken)
+    public function add(array $details, $kbcToken)
     {
+        $this->validateApiDetails($details);
+
         $this->client->post("manage", [
             'headers' => [
                 'X-StorageApi-Token' => $kbcToken
             ],
-            'form_params' => [
-                
-            ]
+            'form_params' => $details
         ]);
     }
 
@@ -52,5 +62,18 @@ class Manager extends Common
     public function listComponents()
     {
         $this->client->get("manage");
+    }
+
+    protected function validateApiDetails(array $details)
+    {
+        foreach($this->requiredApiDetails as $key) {
+            if (empty($details[$key])) {
+                throw new \InvalidArgumentException("Missing key '{$key}'.");
+            }
+        }
+
+        if ($details['oauth_version'] == 1.0 && empty($details['request_token_url'])) {
+            throw new \InvalidArgumentException("Missing 'request_token_url' for OAuth 1.0");
+        }
     }
 }
